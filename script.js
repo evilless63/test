7c668eadedfb1e03c4981577b1817c921fac2cb0
Vitaliy <vitaliy030589@gmail.com>
2019-04-07 18:39:07 +0400

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var leftBorderWidth = 1
+let second = 2
+const pi = 3.14
+
+let something
+console.log(something)
+
+let persone = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+}
+
+console.log(persone.name)
